e3c2e4f0adac3d586371e4c8dbb46c28a01654b1
Sergi Asensio <sergi.asensio.nacher@gmail.com>
2024-08-20 00:05:06 +0200

    starting mysql

---
+++ go.mod
@@ -0,0 +1,14 @@
+module main
+
+go 1.22.6
+
+require (
+	github.com/go-kit/log v0.2.0
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/gorilla/mux v1.8.1
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-    "encoding/json"
-    "github.com/gorilla/mux"
-    log "github.com/go-kit/kit/log"
-    "math/rand"
-    "net/http"
-    "os"
-    "strconv"
-    "time"
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 var logger log.Logger
 
 type GithubOrg struct {
-	OrgName   	string    
-    Token     	string    
-	CreatedAt	time.Time 
+	Name      string    `json:"Name"`
+	Token     string    `json:"Token"`
+	CreatedAt time.Time `json:"CreatedAt,omitempty"`
 }
 
 type Repo struct {
-	Name      	string    
-    GithubOrg	GithubOrg 
-    CreatedAt 	time.Time 
+	Name      string    `json:"Name"`
+	GithubOrg GithubOrg `json:"GithubOrg"`
+	CreatedAt time.Time `json:"CreatedAt,omitempty"`
 }
 
 type PullRequest struct {
-	Id			integer    
-    Repo		Repo
-    CreatedAt 	time.Time 
+	Id        int       `json:"Id"`
+	Repo      Repo      `json:"Repo"`
+	CreatedAt time.Time `json:"CreatedAt,omitempty"`
 	// Reviewed
 	// CI executed
 	// etc
@@ -36,22 +39,103 @@ type PullRequest struct {
 
 // https://github.com/adityajoshi12/restapi-golang/blob/master/main.go
 func main() {
-    r := mux.NewRouter()
+	// Init MySQL
+	InitDb()
 
-    r.HandleFunc("/repo", NewRepo).Methods("POST")
-    http.Handle("/", r)
-    if err := http.ListenAndServe(":3000", r); err != nil {
-        logger.Log("status", "fatal", "err", err)
-        os.Exit(1)
-    }
+	r := mux.NewRouter()
+
+	r.HandleFunc("/org", NewOrg).Methods("POST")
+	r.HandleFunc("/repo", NewRepo).Methods("POST")
+	http.Handle("/", r)
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		logger.Log("status", "fatal", "err", err)
+		os.Exit(1)
+	}
+}
+
+const DBNAME string = "OpenIaCLift"
+
+var db *sql.DB
+
+func InitDb() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   os.Getenv("DBAddress"),
+		DBName: DBNAME,
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		logger := log.NewLogfmtLogger(os.Stdout)
+		level.Error(logger).Log("msg", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE DATABASE " + DBNAME)
+	if err != nil {
+		logger := log.NewLogfmtLogger(os.Stdout)
+		level.Error(logger).Log("msg", err)
+	}
+
+	_, err = db.Exec("USE " + DBNAME)
+	if err != nil {
+		logger := log.NewLogfmtLogger(os.Stdout)
+		level.Error(logger).Log("msg", err)
+	}
+
+	_, err = db.Exec("CREATE TABLE githuborg ( name varchar, token varchar, created_at datetime )")
+	if err != nil {
+		logger := log.NewLogfmtLogger(os.Stdout)
+		level.Error(logger).Log("msg", err)
+	}
+
+	defer db.Close()
+}
+
+func DatabaseExec(query string) error {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   os.Getenv("DBAddress"),
+		DBName: DBNAME,
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		logger := log.NewLogfmtLogger(os.Stdout)
+		level.Error(logger).Log("msg", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE DATABASE " + DBNAME)
+	if err != nil {
+		logger := log.NewLogfmtLogger(os.Stdout)
+		level.Error(logger).Log("msg", err)
+		return os.NewSyscallError(err.Error(), err)
+	}
+	return nil
 }
 
 func NewOrg(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("content-type", "application/json")
-    json.NewEncoder(w).Encode(posts)
+	w.Header().Set("content-type", "application/json")
+	var g GithubOrg
+	err := json.NewDecoder(r.Body).Decode(&g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
 
 func NewRepo(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("content-type", "application/json")
-    json.NewEncoder(w).Encode(posts)
+	w.Header().Set("content-type", "application/json")
+	// json.NewEncoder(w).Encode(posts)
 }
