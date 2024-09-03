01fffbf5064a58ccb2e716bca2eadfa37388cfec
Sergi Asensio <sergi.asensio.nacher@gmail.com>
2024-09-03 23:53:57 +0200

    mysql is working, todo added

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// TODO: Estructura de carpetas (db en internal, setup, etc)
 package main
 
 import (
@@ -10,7 +11,6 @@ import (
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/go-sql-driver/mysql"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -23,8 +23,8 @@ type GithubOrg struct {
 }
 
 type Repo struct {
+	Id        int       `json:"Id"`
 	Name      string    `json:"Name"`
-	GithubOrg GithubOrg `json:"GithubOrg"`
 	CreatedAt time.Time `json:"CreatedAt,omitempty"`
 }
 
@@ -55,40 +55,41 @@ func main() {
 
 const DBNAME string = "OpenIaCLift"
 
-var db *sql.DB
-
-func InitDb() {
-	// Capture connection properties.
-	cfg := mysql.Config{
+var (
+	db  *sql.DB
+	cfg = mysql.Config{
 		User:   os.Getenv("DBUSER"),
 		Passwd: os.Getenv("DBPASS"),
 		Net:    "tcp",
 		Addr:   os.Getenv("DBAddress"),
 		DBName: DBNAME,
 	}
+	dbConnectionString = cfg.User + ":" + cfg.Passwd + "@" + cfg.Net + "(" + cfg.Addr + ")/"
+)
 
+func InitDb() {
 	// Get a database handle.
 	var err error
-	db, err = sql.Open("mysql", cfg.FormatDSN())
+	db, err = sql.Open("mysql", dbConnectionString)
+	if err != nil {
+		logger := log.NewLogfmtLogger(os.Stdout)
+		level.Error(logger).Log("msg", err)
+		os.Exit(-1)
+	}
+	_, err = db.Exec("CREATE DATABASE " + cfg.DBName)
 	if err != nil {
 		logger := log.NewLogfmtLogger(os.Stdout)
 		level.Error(logger).Log("msg", err)
 	}
-	defer db.Close()
-
-	_, err = db.Exec("CREATE DATABASE " + DBNAME)
+	_, err = db.Exec("USE " + cfg.DBName)
 	if err != nil {
 		logger := log.NewLogfmtLogger(os.Stdout)
 		level.Error(logger).Log("msg", err)
 	}
-
-	_, err = db.Exec("USE " + DBNAME)
-	if err != nil {
-		logger := log.NewLogfmtLogger(os.Stdout)
-		level.Error(logger).Log("msg", err)
-	}
-
-	_, err = db.Exec("CREATE TABLE githuborg ( name varchar, token varchar, created_at datetime )")
+	// TODO: Create all tables
+	// TODO: Acordarse de id + autoincrement
+	// TODO: Antes hacer un poco de dibujo
+	_, err = db.Exec("CREATE TABLE githuborg ( name varchar(255), token varchar(255), created_at datetime )")
 	if err != nil {
 		logger := log.NewLogfmtLogger(os.Stdout)
 		level.Error(logger).Log("msg", err)
